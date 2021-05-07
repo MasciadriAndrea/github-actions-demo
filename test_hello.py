3433bd64e1ce9a6fbc8f863ca51f5322771cf2ba
root <masciadri.andrea@gmail.com>
2021-05-07 12:48:28 +0000

    fix

--- test_hello.py
+++ test_hello.py
@@ -1,8 +1,8 @@
-from hello import add, my_sub
+from hello import add, sub
 
 
 def test_add():
     assert 2 == add(1, 1)
 
 def test_sub():
-    assert 0 == my_sub(1, 1) 
+    assert 0 == sub(1, 1) 
